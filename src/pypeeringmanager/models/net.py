5cd1ae1e5e84b10c559e55aa47faf2e6be56b0cc
Arnoud Vermeer <arnoud.vermeer@exoscale.com>
2025-09-12 15:56:14 +0200

    Another circular dependency

--- src/pypeeringmanager/models/net.py
+++ src/pypeeringmanager/models/net.py
@@ -1,7 +1,6 @@
-from pynetbox.core.response import Record
-from pypeeringmanager.models.peering import InternetExchanges
-from pypeeringmanager.models.devices import Routers
+class Connections:
+    from pypeeringmanager.models.peering import InternetExchanges
+    from pypeeringmanager.models.devices import Routers
 
-class Connections(Record):
     internet_exchange_point = InternetExchanges
-    router =  Routers
+    router = Routers
