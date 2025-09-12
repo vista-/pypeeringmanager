a12feaf7f655de89fc86dbc5823c0ed733fe7dfb
Arnoud Vermeer <arnoud.vermeer@exoscale.com>
2025-09-12 16:04:17 +0200

    More circular dependencies

--- src/pypeeringmanager/models/peering.py
+++ src/pypeeringmanager/models/peering.py
@@ -1,7 +1,5 @@
 from pynetbox.core.response import Record
-from pypeeringmanager.models.bgp import Relationships
 from pypeeringmanager.models.extras import Tags
-from pypeeringmanager.models.net import Connections
 
 class RoutingPolicies(Record):
     pass
@@ -36,7 +34,6 @@ class DirectPeeringSessions(Record):
     bgp_group = BGPGroups
     import_routing_policies = RoutingPolicies
     export_routing_policies = RoutingPolicies
-    relationship = Relationships
     tags = Tags
 
     @property
@@ -44,16 +41,25 @@ class DirectPeeringSessions(Record):
         from pypeeringmanager.models.devices import Routers
         return Routers
 
+    @property
+    def relationship(self):
+        from pypeeringmanager.models.bgp import Relationships
+        return Relationships
+
     def __str__(self):
         return self.ip_address
 
 
 class InternetExchangePeeringSessions(Record):
     autonomous_system = AutonomousSystems
-    ixp_connection = Connections
     import_routing_policies = RoutingPolicies
     export_routing_policies = RoutingPolicies
     tags = Tags
+
+    @property
+    def ixp_connection(self):
+        from pypeeringmanager.models.net import Connections
+        return Connections
 
     def __str__(self):
         return self.ip_address
